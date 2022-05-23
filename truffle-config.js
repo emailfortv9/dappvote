79c8708e1de3f972c3d95b73126193201e4f8cdf
Ajith Kumar <ajithvnr2001@gmail.com>
2022-05-23 17:45:57 +0530

    commit

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+//const fs = require('fs');
+const mnemonic = require("./secrets.json").mnemonic;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://matic-mumbai.chainstacklabs.com	`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+    }
+  }
+}
